6660a737b2d3166711828bfbd7d6c25b61f2bb8f
KirillMekenya <mekenya93@gmail.com>
2017-10-11 14:54:57 +0300

    build v.3

--- task3/src/com/company/AssemblyLine.java
+++ task3/src/com/company/AssemblyLine.java
@@ -1,10 +1,19 @@
 package com.company;
 
 public class AssemblyLine implements IAssemblyLine{
+    LineStep step1;
+    LineStep step2;
+    LineStep step3;
+    public AssemblyLine(LineStep step1, LineStep step2, LineStep step3)
+    {
+        this.step1=step1;
+        this.step2=step2;
+        this.step3=step3;
+    }
     public Product assembleProduct(Product product){
-        product.installFirstPart(product.part1);
-        product.installSecondPart(product.part2);
-        product.installThirdPart(product.part3);
+        product.installFirstPart(step1.buildProductPart());
+        product.installSecondPart(step2.buildProductPart());
+        product.installThirdPart(step3.buildProductPart());
         System.out.println("Сборка продукта " +product.name+" завершена.");
         return product;
     }
--- task3/src/com/company/Main.java
+++ task3/src/com/company/Main.java
@@ -5,17 +5,14 @@ public class Main {
        ProductPart part1 = new ProductPart("корпус");
        ProductPart part2 = new ProductPart("материнская плата");
        ProductPart part3 = new ProductPart("монитор");
-       AssemblyLine realization = new AssemblyLine();
        LineStep process1 = new LineStep();
        process1.productPart=part1;
        LineStep process2 = new LineStep();
        process2.productPart = part2;
        LineStep process3 = new LineStep();
        process3.productPart = part3;
+       AssemblyLine realization = new AssemblyLine(process1, process2, process3);
        Product product = new Product("Ноутбук");
-       product.part1=process1.buildProductPart();
-       product.part2=process2.buildProductPart();
-       product.part3=process3.buildProductPart();
        realization.assembleProduct(product);
     }
 }
